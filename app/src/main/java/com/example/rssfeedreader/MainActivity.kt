cd1781433a0cb6b8390a9cb5b654092c3b199a01
Rajput Kapilesh <rajputkapilesh@outlook.com>
2021-02-09 14:16:40 +0530

    Added code to download XML-Data From Apple RSS Feed (old_way_of_doing_things)
    This is done using old way of doing things and is not recomended. It's done
    only for better understanding of the whole process of downloading data.

--- app/src/main/java/com/example/rssfeedreader/MainActivity.kt
+++ app/src/main/java/com/example/rssfeedreader/MainActivity.kt
@@ -3,10 +3,83 @@ package com.example.rssfeedreader
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: called")
+
+        val downloadData = DownloadData()
+
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        Log.d(TAG, "onCreate: done")
     }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${params[0]}")
+                val rssFeed = downloadXML(params[0]!!)
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error Downloading")
+                }
+                return rssFeed
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: parameter is $result")
+            }
+
+            private fun downloadXML(urlPath: String): String {
+                val xmlResult = StringBuilder()
+                try {
+                    val url = URL(urlPath)
+                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    val response = connection.responseCode
+                    Log.d(TAG, "downloadXML: The response code was: $response")
+
+//                    val inputStream = connection.inputStream
+//                    val inputStreamReader = InputStreamReader(inputStream)
+//                    val reader = BufferedReader(inputStreamReader)
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val inputBuffer = CharArray(500)
+                    var charsRead = 0
+                    while (charsRead >= 0) {
+                        charsRead = reader.read(inputBuffer)
+                        if (charsRead > 0) {
+                            xmlResult.append(String(inputBuffer, 0, charsRead))
+                        }
+                    }
+                    reader.close()
+
+                    Log.d(TAG, "downloadXML: Received ${xmlResult.length} bytes")
+                    return xmlResult.toString()
+                } catch (e: MalformedURLException) {
+                    Log.e(TAG, "downloadXML: Invalid URL: ${e.message}")
+                } catch (e: IOException) {
+                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+                } catch (e: SecurityException) {
+                    Log.e(TAG, "downloadXML: Security exception: Needs permissions? ${e.message}")
+                } catch (e: Exception) {
+                    Log.e(TAG, "downloadXML: Unknown error: ${e.message}")
+                }
+                return ""
+            }
+        }
+    }
+
+
 }
