230b3c95168b117924b7c1b8c2784d600d85ce4b
Rajput Kapilesh <rajputkapilesh@outlook.com>
2021-02-09 16:44:53 +0530

    Added UI elemets (ListView) to show downloaded & parsed data
    1. Added ListView & ArrayAdapter to show list of downloaded FeedEntries.
       Files Added: list_item.xml
    2. Added viewBinding

--- app/src/main/java/com/example/rssfeedreader/MainActivity.kt
+++ app/src/main/java/com/example/rssfeedreader/MainActivity.kt
@@ -1,27 +1,45 @@
 package com.example.rssfeedreader
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.example.rssfeedreader.databinding.ActivityMainBinding
 import java.net.URL
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Log.d(TAG, "onCreate: called")
 
-        val downloadData = DownloadData()
+        val downloadData = DownloadData(this, binding.xmlListView)
 
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml")
         Log.d(TAG, "onCreate: done")
     }
 
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>() {
+        private class DownloadData(context: Context, listView: ListView) :
+            AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
+
             override fun doInBackground(vararg params: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${params[0]}")
                 val rssFeed = downloadXML(params[0]!!)
@@ -35,6 +53,13 @@ class MainActivity : AppCompatActivity() {
                 super.onPostExecute(result)
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(
+                    propContext,
+                    R.layout.list_item,
+                    parseApplications.applications
+                )
+                propListView.adapter = arrayAdapter
             }
 
             private fun downloadXML(urlPath: String): String {
