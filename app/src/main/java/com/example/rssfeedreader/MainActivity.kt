c2a74cab66d292b45c6f2c19abfc7dbe7c45ac14
Rajput Kapilesh <rajputkapilesh@outlook.com>
2021-02-09 13:02:19 +0530

    Empty project with all the defaults

---
+++ app/src/main/java/com/example/rssfeedreader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rssfeedreader
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
